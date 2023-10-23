59a9454806ac3c50fd424f56bcc633f2bda8f46b
gektor558 <danil.trigub@gmail.com>
2023-10-23 20:42:17 +0200

    base

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,32 @@
+import styles from './FriendsList.module.css';
+
+const FriendListItem = ({ avatar, name, isOnline = false }) => {
+  return (
+    <li className={styles.item}>
+      <span
+        className={`${styles.status}  ${
+          isOnline ? styles.online : styles.offline
+        }`}
+      ></span>
+      <img className={styles.avatar} src={avatar} alt={name} width="48" />
+      <p className={styles.name}>{name}</p>
+    </li>
+  );
+};
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={styles.friendList}>
+      {friends.map(friend => {
+        return (
+          <FriendListItem
+            key={friend.id}
+            isOnline={friend.isOnline}
+            avatar={friend.avatar}
+            name={friend.name}
+          />
+        );
+      })}
+    </ul>
+  );
+};
